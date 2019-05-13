3e12fd364935ecacbb5448c1ae513fa57936932c
Saurabh Srivastava <saurabhs@uchicago.edu>
2019-05-13 11:51:17 -0500

    updated

--- Main.R
+++ Main.R
@@ -16,6 +16,27 @@ library(funModeling)
 
 library(psych)
 
+library(glmulti) #Select the best linear regression model
+library(lubridate) #Date Time Conversion
+library(BBmisc) #Convert Rows to List
+library(pROC) #ROC
+library(DMwR) #Error Estimates
+library(readr)
+library(caret)
+library(nlme)
+library(magrittr)
+library(stringr)
+library(tidyverse)
+library(randomForest)
+library(e1071)
+library(Metrics)
+
+
+
+
+
+
+
 input_dir<-"F:/Projects/TimeSeries/EndGame/"
 
 csv_files = list.files(input_dir, recursive = T, full.names = T)
@@ -239,3 +260,99 @@ plot_histogram(numericalData)
 
 
 plot_density(numericalData)
+
+
+clean_elements_function = function(x){
+  x = str_squish(x)
+  x = gsub("[{}]", "", x)
+  x = gsub("'", '', x)
+}
+
+count_function = function(x){
+  value = length(unlist(strsplit(as.character(x), ",")))
+}
+
+last_element_list_function = function(x){
+  value = unlist(strsplit(as.character(x), ","))
+  value = as.character(value[length(value)])
+  clean_elements_function(value)
+}
+
+count_characters_function = function(x){
+  x = unlist(x)
+  x = nchar(str_squish(x), type = "chars")
+}
+
+
+market_data_preparation = function(market){
+  
+  market$time = as.Date(market$time)
+  
+  volume_to_mean = market['volume'] / mean(market$volume)
+  volume_to_mean = as.data.frame(volume_to_mean)
+  colnames(volume_to_mean) = c("volume_to_mean")
+  market = cbind(market,(volume_to_mean))
+  
+  returnsOpenPrevRaw1_to_volume = market['returnsOpenPrevRaw1'] / market['volume']
+  returnsOpenPrevRaw1_to_volume = as.data.frame(returnsOpenPrevRaw1_to_volume)
+  colnames(returnsOpenPrevRaw1_to_volume) = c("returnsOpenPrevRaw1_to_volume")
+  market = cbind(market,(returnsOpenPrevRaw1_to_volume))
+  
+  close_to_open = market['close'] / market['open']
+  close_to_open = as.data.frame(close_to_open)
+  colnames(close_to_open) = c("close_to_open")
+  market = cbind(market,(close_to_open))
+  
+  return((market))
+}
+
+news_data_preparation = function(news){
+  news['sentence_word_count'] =  news['wordCount'] / news['sentenceCount']
+  news['time']= hour(news$time)
+  news['sourceTimestamp']= hour(news$sourceTimestamp)
+  news['firstCreated']= as.Date(news$firstCreated)
+  
+  asset.codes.list = convertRowsToList(news['assetCodes'])
+  asset.codes.len  = t(as.data.frame(lapply(asset.codes.list, count_function)))
+  colnames(asset.codes.len) = c("assetCodesLen")
+  news = cbind(news,asset.codes.len)
+  news['assetCodes'] = t(as.data.frame(lapply(asset.codes.list, last_element_list_function)))
+  asset.codes.list.new = convertRowsToList(news['assetCodes'])
+  news['assetCodesLen'] = t(as.data.frame(lapply(asset.codes.list.new, count_characters_function)))
+  
+  headlines.list = convertRowsToList(news['headline'])
+  headlines.len = t(as.data.frame(lapply(headlines.list, count_characters_function)))
+  colnames(headlines.len) = c("headlinesLen")
+  news = cbind(news,headlines.len)
+  
+  asset.sentiment.count.dataframe = news%>%
+    group_by(assetName,sentimentClass)%>%
+    select(time)%>%
+    tally
+  new.column = asset.sentiment.count.dataframe[match(news$assetName,asset.sentiment.count.dataframe$assetName),3]
+  asset.sentiment.count = as.data.frame(new.column)
+  colnames(asset.sentiment.count) = c("assetSentimentCount")
+  news = cbind(news,asset.sentiment.count)
+  
+  df_old = as.data.frame(news['headlineTag'])
+  a = convertRowsToList(unique(df_old['headlineTag']))
+  b = unlist(a)
+  x = as.array(clean_elements_function(b))
+  y = seq(0,length(x)-1)
+  df = as.data.frame(cbind(x,y))
+  new.column = df[match(df_old$headlineTag, df$x),2]
+  headline.Tag.T = as.data.frame(new.column)
+  colnames(headline.Tag.T) = c("headlineTagT")
+  news = cbind(news,headline.Tag.T)
+  
+  colnames(news)[which(names(news) == "assetName")] = "assetNames"
+  colnames(news)[which(names(news) == "time")] = "time.news"
+  return(news)
+}
+
+combined_data_preparation = function(market,news){
+  combined_df = merge(x=market,y=news,by.x = c("time", "assetName"), by.y =c("firstCreated", "assetNames"))
+  combined_df = na.omit(combined_df)
+  return(combined_df)
+}
+
